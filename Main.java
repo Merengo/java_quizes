35c174db6d57346d1409d0d1549cb3c4c149cce4
Merengo <marwamerengo@gmail.com>
2022-10-28 11:25:05 +0300

    in progress to learn the objects

--- Main.java
+++ Main.java
@@ -1,12 +1,21 @@
 class Test   
 {  
-    int i;   
+    Test(){
+        int i =5;
+        char x = 'y';
+    }   
 }  
 public class Main   
 {  
     public static void main (String args[])   
     {  
-        Test test = new Test();   
-        System.out.println(test);  
+        Test test = new Test();
+        for (Field field : test.getClass().getDeclaredFields()) {
+        field.setAccessible(true);
+        String name = field.getName();
+        Object value = field.get(test);
+        System.out.printf("%s: %s%n", name, value);
+}   
+        // System.out.println(test);  
     }  
 }
